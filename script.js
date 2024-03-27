a3835819896365bb6969117d9c55031443134842
Natalia Rodrigues <nanaz2007costa@gmail.com>
2024-03-27 20:52:04 -0300

    Quarto commit do JS

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ botoesGorjeta.forEach (botao=>{
 })
 
 
-function receberPorcentagemBotao(evento){
+function receberPorcentagem(evento){
     botoesGorjeta.forEach(botao=>{
         botao.classList.remote("botao-ativo")
 
@@ -39,7 +39,16 @@ function receberPorcentagemBotao(evento){
             botao.classList.add("botao-ativo")
         }
     })
-    
-   porcentagem= parseFloat(evento.target.value)/100
+
+    if (evento.target.value !==""){
+        porcentagem= parseFloat(evento.target.value)/100
+    } else {
+        porcentagem =0
+    }
+   console.log(porcentagem)
 
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
+
