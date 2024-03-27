c196a42ce3d2fa406999c72780105b5bead59b97
Natalia Rodrigues <nanaz2007costa@gmail.com>
2024-03-27 20:27:34 -0300

    Setimo commit do HTML e primeiro commit do JS

---
+++ script.js
@@ -0,0 +1,29 @@
+let conta = 0
+let pessoas = 0
+
+
+const contaInput= document.querySelector(".conta input")
+contaInput .addEventListener("input",)
+
+function receberValorConta(evento){
+    conta = Number(evento.target.value)
+}
+
+const pessoaaInput= document.querySelector("pessoas")
+pessoasInput.addEventListener("input",receberQuantidadeDePessoas)
+
+
+function receberQuantidadeDePessoas(evento){
+    const paragrafoErro= document.querySelector("pessoas #erro")
+    const divErro= document.querySelector(".pessoas .input-box")
+
+    if(evento.target.value ==="0"){
+        paragrafoErro.style.display = "block"
+        divErro.setAttribute("id", "erro-div")
+    }else{
+        paragrafoErro.style.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas = Number(evento.target.value)
+    }
+
+}
